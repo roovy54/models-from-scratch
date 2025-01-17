48e8c1182f2e3eeeb8826732041636472052734b
Harshith M R <roovy54@gmail.com>
2025-01-17 15:37:16 +0530

    Update GANs

--- GAN/WGAN/WGAN.py
+++ GAN/WGAN/WGAN.py
@@ -27,7 +27,7 @@ class Discriminator(nn.Module):
                 padding,
                 bias=False,
             ),
-            nn.InstanceNorm2d(out_channels),
+            nn.InstanceNorm2d(out_channels, affine=True),
             nn.LeakyReLU(0.2),
         )
 
